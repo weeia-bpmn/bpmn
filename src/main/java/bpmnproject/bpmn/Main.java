856dd1afe7225b594b812095f6dbc33a5437d821
Dawid Świnoga <dawid.swinoga@gmail.com>
2018-05-15 17:48:20 +0200

    init

---
+++ src/main/java/bpmnproject/bpmn/Main.java
@@ -0,0 +1,17 @@
+package bpmnproject.bpmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Hello world!
+ *
+ */
+
+@EnableAutoConfiguration
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Main.class, args);
+	}
+}
