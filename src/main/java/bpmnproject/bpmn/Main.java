5aea8e5f0ec2c503b816135177e214bc0b741043
Piotr Borczyk <howorang@gmail.com>
2018-05-22 01:03:48 +0200

    WarrantyRepair.bpmn auto deploy

--- src/main/java/bpmnproject/bpmn/Main.java
+++ src/main/java/bpmnproject/bpmn/Main.java
@@ -1,14 +1,14 @@
 package bpmnproject.bpmn;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
 
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) throws Exception {
