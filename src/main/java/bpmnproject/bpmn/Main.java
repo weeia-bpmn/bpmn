0eb0af0c6197bcd4983bfbd190fb76c5f20d8c71
Dawid Świnoga <dawid.swinoga@gmail.com>
2018-05-28 00:34:18 +0200

    Add postresql and fix bpmn configuration

--- src/main/java/bpmnproject/bpmn/Main.java
+++ src/main/java/bpmnproject/bpmn/Main.java
@@ -1,7 +1,12 @@
 package bpmnproject.bpmn;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 /**
  * Hello world!
@@ -9,9 +14,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableProcessApplication
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+	}
+
+	@Autowired
+	private RuntimeService runtimeService;
+
+	@EventListener
+	public void processPostDeploy(PostDeployEvent event) {
+		runtimeService.startProcessInstanceByKey("warrantyService");
 	}
 }
