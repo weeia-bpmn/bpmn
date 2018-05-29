c1c7ece8615f7cbf9b9dd9ca264b63d957df9c51
Dawid Świnoga <dawid.swinoga@gmail.com>
2018-05-29 18:29:48 +0200

    Add client pay for repair implementation.

---
+++ src/main/java/bpmnproject/bpmn/delegate/ClientPayRepairEndMessageDelegate.java
@@ -0,0 +1,47 @@
+package bpmnproject.bpmn.delegate;
+
+import bpmnproject.bpmn.mail.MailService;
+import lombok.RequiredArgsConstructor;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+import java.time.ZoneOffset;
+import java.util.Date;
+
+/**
+ * Created by Dawid on 29.05.2018 at 18:13.
+ */
+@Component("clientPayRepairEndMessageDelegate")
+@RequiredArgsConstructor
+public class ClientPayRepairEndMessageDelegate implements JavaDelegate {
+    private final MailService mailService;
+
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        String customerEmail = (String) execution.getVariable("customerEmail");
+
+        String content = createContent(execution);
+
+        mailService.sendMessage(customerEmail, "Naprawa telefonu - serwis gwarancyjny", content);
+
+    }
+
+    private String createContent(DelegateExecution execution) {
+        Date date = (Date) execution.getVariable("phoneDeliveryDate");
+        StringBuilder content = new StringBuilder();
+        content.append("Telefon zostnie naprawiony. Będzie gotowy od odbioru od dnia: ")
+                .append(date.toInstant().atOffset(ZoneOffset.UTC).toLocalDate().toString());
+
+        String infoForClient = (String) execution.getVariable("infoForClient");
+
+        if (infoForClient != null) {
+            content.append("\n\nWykonane czynności:\n").append(infoForClient);
+        }
+
+        Long cost = (Long) execution.getVariable("cost");
+        content.append("\n\n").append("Koszt naprawy: ").append(cost).append(" PLN");
+        return content.toString();
+    }
+}
